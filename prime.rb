f80cee229bfd6125007ff67ed7341ba6e37008f7
AfraaAlobaid <afraa.obaid1@gmail.com>
2022-01-14 21:02:31 +1030

    Used nested if statements to check prime num

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(x)
+    if x <= 1 
+        return false 
+    else
+        puts x
+        #puts (2..x-1).to_a.inspect
+        (2..x-1).to_a.each do |y|
+            if x % y == 0
+                return false
+            end
+        end 
+    end
+    return true
+end
